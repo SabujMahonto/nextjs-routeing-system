f838366bd5ee8c57d2204105b7a7bf28c152cf85
sabujmahonto <sabujmahonto@gmail.com>
2023-04-24 10:22:59 +0600

    simple navigation done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div><h1>HomePage</h1></div>
+    <div><h1>HomePage</h1>
+    <ul>
+      <li><Link href="/products">Product Page</Link></li>
+      <li><Link href="/blog">Blog Page</Link></li>
+      <li><Link href="/library">Library</Link></li>
+    </ul>
+    </div>
   )
 }
