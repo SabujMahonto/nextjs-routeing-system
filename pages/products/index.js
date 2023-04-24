c456fe5b2c1269ba6e7a96bea3427db16e711903
sabujmahonto <sabujmahonto@gmail.com>
2023-04-24 10:52:06 +0600

    dynamic product or product Id done

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,17 +1,18 @@
 import Link from "next/link";
 
 
-export default function ProductList() {
+export default function ProductList({productId = 99}) {
   return (
     <div>
         <h1>ProductList</h1>
         <Link href="/">Backt to Home</Link>
         <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
-            <li>Product 4</li>
-            <li>Product 5</li>
+            <li><Link href="/products/1">Product 1</Link></li>
+            <li><Link href="/products/2">Product 2</Link></li>
+            <li><Link href="/products/3">Product 3</Link></li>
+            <li><Link href="/products/4">Product 4</Link></li>
+            <li><Link href="/products/5">Product 5</Link></li>
+            <li><Link href={`/products/${productId}`}>Product {productId}</Link></li>
         </ul>
     </div>
   )
